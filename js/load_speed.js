36ce5c3df865f53a826cd3ed807a9bd52b21e4fd
zzucch <nikitamilovatskiy@gmail.com>
2024-11-02 13:58:58 +0300

    fix: remove deprecated functions from load time calculation

--- js/load_speed.js
+++ js/load_speed.js
@@ -1,11 +1,12 @@
+const startTime = performance.now();
+
 (function () {
   window.onload = function () {
-    const loadTimeMs =
-      window.performance.timing.domComplete -
-      window.performance.timing.requestStart;
+    const endTime = performance.now();
+    const loadTimeMs = endTime - startTime;
 
     const loadTimeInfo = document.createElement("div");
-    loadTimeInfo.innerText = `Page loaded in ${loadTimeMs} ms`;
+    loadTimeInfo.innerText = `Page loaded in ${loadTimeMs.toFixed(2)} ms`;
     loadTimeInfo.style.backgroundColor = "#222";
     loadTimeInfo.style.color = "#aaa";
     loadTimeInfo.style.padding = "10px";
